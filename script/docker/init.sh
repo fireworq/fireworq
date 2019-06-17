939bacdf0a4677a49deac8607e8395496d7239c6
tarao <tarao.gnn@gmail.com>
2019-06-17 16:00:01 +0900

    Missing environment variables when there is docker-compose installed.

--- script/docker/init.sh
+++ script/docker/init.sh
@@ -2,6 +2,6 @@ type docker-compose >/dev/null || {
     curl -L https://github.com/docker/compose/releases/download/1.11.2/run.sh > docker-compose
     chmod +x docker-compose
     DOCKER_COMPOSE='./docker-compose'
-    export COMPOSE_OPTIONS="-e USER_ID=$(id -ur) -e GROUP_ID=$(id -gr) -e GOOS=$GOOS -e GOARCH=$GOARCH"
 }
+export COMPOSE_OPTIONS="-e USER_ID=$(id -ur) -e GROUP_ID=$(id -gr) -e GOOS=$GOOS -e GOARCH=$GOARCH"
 DOCKER_COMPOSE=${DOCKER_COMPOSE:-docker-compose}
