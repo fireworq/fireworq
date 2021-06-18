dfc5c04a38f1086bff829267717312a615225aab
yigarashi <yigarashi@hatena.ne.jp>
2021-06-18 10:38:32 +0900

    Add omitempty tag to ignorable fields

--- model/model.go
+++ model/model.go
@@ -3,10 +3,10 @@ package model
 // Queue describes a queue.
 type Queue struct {
 	Name                   string  `json:"name"`
-	PollingInterval        uint    `json:"polling_interval"`
+	PollingInterval        uint    `json:"polling_interval,omitempty"`
 	MaxWorkers             uint    `json:"max_workers"`
-	MaxDispatchesPerSecond float64 `json:"max_dispatches_per_second"`
-	MaxBurstSize           uint    `json:"max_burst_size"`
+	MaxDispatchesPerSecond float64 `json:"max_dispatches_per_second,omitempty"`
+	MaxBurstSize           uint    `json:"max_burst_size,omitempty"`
 }
 
 // Routing describes a routing.
